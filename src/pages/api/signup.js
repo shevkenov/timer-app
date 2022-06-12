87d216d6289794281d07d585af6302d250b359c5
Andrey SHevkenov <shevkenov@gmail.com>
2022-06-12 11:44:49 +0300

    add: sigup route

---
+++ src/pages/api/signup.js
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createUser } from "../../utils/harperDB/createNewUser";
+
+export default async function handler(req, res) {
+  const {username, password, repeatPassword} = req.body;
+
+  try {
+    if(!username || !password || !repeatPassword){
+      throw new Error("All fields are reuqired!");
+    }
+    
+    if(password !== repeatPassword){
+      throw new Error("Both passwords do not match");
+    }
+
+    const {response, result} = await createUser({method: req.method, data: {username, password}});
+    if(response.status !== 200) {
+      throw new Error(result.error);
+    }
+    
+    res.status(response.status).json({ data: result });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+}
