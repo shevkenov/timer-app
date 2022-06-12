37ea79e12807309d5267efa79fe41c82c8d8168c
Andrey SHevkenov <shevkenov@gmail.com>
2022-06-12 11:44:07 +0300

    add: handle errors

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,22 +1,40 @@
-import { useState } from 'react'
-import { LabelAndInput } from './layout/Form'
-import Button from "./Button"
+import { useState } from 'react';
+import { LabelAndInput } from './layout/Form';
+import postFormData from "../utils/formData";
+import Button from "./Button";
+import Alert from './Alert';
 
 const SignupForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [repeatPassword, setRepeatPassword] = useState("");
+    const [error, setError] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
+        setError("");
         e.preventDefault();
+        const credentials = {
+            username,
+            password,
+            repeatPassword
+        }
+        const response = await postFormData({data: credentials, url: "/api/signup", method: "POST"})
+        if(response.error){
+          setError(response.error);
+        }
+        console.log(response);
+        
     }
 
   return (
     <form onSubmit={handleSubmit}>
         <LabelAndInput label="Username" value={username} handleChange={(e) => setUsername(e.target.value)} inputType="text" inputName="username"/>
         <LabelAndInput label="Password" value={password} handleChange={(e) => setPassword(e.target.value)} inputType="password" inputName="password"/>
-        <LabelAndInput label="Confirm Password" value={repeatPassword} handleChange={(e) => setRepeatPassword(e.target.value)} inputType="password" inputName="password"/>
+        <LabelAndInput label="Confirm Password" value={repeatPassword} handleChange={(e) => setRepeatPassword(e.target.value)} inputType="password" inputName="password1"/>
         <Button extraClasses="w-full mt-3 py-3 font-semibold" color="success">Create Account</Button>
+        {
+          error && <Alert type="danger">{error}</Alert>
+        }
     </form>
   )
 }
