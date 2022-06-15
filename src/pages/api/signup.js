a24a13f134effdefddb36b51219840ce4f0e70db
Andrey SHevkenov <shevkenov@gmail.com>
2022-06-15 19:22:30 +0300

    fix: signup comp

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -1,6 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { createUser } from "../../utils/harperDB/createNewUser";
-import { harperFetchToken } from "../../utils/harperDB/harperFetchToken";
 
 export default async function handler(req, res) {
   const {username, password, repeatPassword} = req.body;
